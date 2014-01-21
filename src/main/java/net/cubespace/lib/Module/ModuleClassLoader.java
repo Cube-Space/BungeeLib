6b49d934e6f17e75faa82f64a1baad4887000b11
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-01-21 17:35:36 +0100

    Fixed ModuleClassloader not seeing other Plugins

--- src/main/java/net/cubespace/lib/Module/ModuleClassLoader.java
+++ src/main/java/net/cubespace/lib/Module/ModuleClassLoader.java
@@ -1,5 +1,7 @@
 package net.cubespace.lib.Module;
 
+import net.cubespace.lib.CubespacePlugin;
+
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Set;
@@ -9,9 +11,12 @@ import java.util.concurrent.CopyOnWriteArraySet;
  * @author geNAZt (fabian.fassbender42@googlemail.com)
  */
 public class ModuleClassLoader extends URLClassLoader {
+    private static final ClassLoader parentLoader;
     private static final Set<ModuleClassLoader> allLoaders = new CopyOnWriteArraySet<>();
 
     static {
+        parentLoader = CubespacePlugin.class.getClassLoader();
+
         ClassLoader.registerAsParallelCapable();
     }
 
@@ -27,19 +32,27 @@ public class ModuleClassLoader extends URLClassLoader {
 
     private Class<?> loadClass0(String name, boolean resolve, boolean checkOther) throws ClassNotFoundException {
         try {
-            return super.loadClass( name, resolve );
-        } catch ( ClassNotFoundException ex ) { }
+            return super.loadClass(name, resolve);
+        } catch (ClassNotFoundException ex) {
+        }
 
         if (checkOther) {
-            for ( ModuleClassLoader loader : allLoaders ) {
-                if ( loader != this ) {
+            for (ModuleClassLoader loader : allLoaders) {
+                if (loader != this) {
                     try {
-                        return loader.loadClass0( name, resolve, false );
-                    } catch ( ClassNotFoundException ex ) { }
+                        return loader.loadClass0(name, resolve, false);
+                    } catch (ClassNotFoundException ex) {
+                    }
                 }
+            }
+
+            //Check parent loader
+            try {
+                return parentLoader.loadClass(name);
+            } catch (ClassNotFoundException ex) {
             }
         }
 
-        throw new ClassNotFoundException( name );
+        throw new ClassNotFoundException(name);
     }
 }
